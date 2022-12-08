ea4b30f38e524b85ea332e81a07ee2124af077bc
LimjooHyun <99496403+uhzill@users.noreply.github.com>
2022-12-09 01:36:14 +0900

    2

--- scripts.js
+++ scripts.js
@@ -20,20 +20,20 @@ $(document).ready( function() {
 
 
 
-    // ABOUT ME
+    // ABOUT ME 듀얼 슬라이드
     var Panels = (function() {
 
-        var panelLeft = document.querySelector('.panels_side--left');
-        var panelRight = document.querySelector('.panels_side--right');
+        var panelLeft = document.querySelector('.--left');
+        var panelRight = document.querySelector('.--right');
         
         var openLeft = function() {
-                panelLeft.classList.toggle('panels_side--left-active');
-            panelRight.classList.toggle('panels_side--right-hidden');
+                panelLeft.classList.toggle('--left-active');
+            panelRight.classList.toggle('--right-hidden');
         };
         
         var openRight = function() {
-            panelRight.classList.toggle('panels_side--right-active');
-            panelLeft.classList.toggle('panels_side--left-hidden');
+            panelRight.classList.toggle('--right-active');
+            panelLeft.classList.toggle('--left-hidden');
         };
         
         var bindActions = function() {
