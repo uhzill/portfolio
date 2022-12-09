de2530d43eac72c3e1d8ef488052361e85b812c1
LimjooHyun <99496403+uhzill@users.noreply.github.com>
2022-12-09 16:31:54 +0900

    8

--- js/works.js
+++ js/works.js
@@ -1,11 +1,18 @@
 $(document).ready( function() {
 
     // 프로젝트 소개
-    $(".project_text").on("click", function () {
+    $(".project_list").on("click", function () {
+
+        $(this).find('.popup_box').addClass('active');
+        $(this).find('.popup_box_wrap').css('zIndex','0');
+
+    });
+
+    $(".popup_button").on("click", function () {
 
         console.log("클릭");
-        $('.popup_box').addClass('active');
-        $('.popup_box_wrap').css('zIndex','0');
+        $(this).has('.popup_box').removeClass('active');
+        $(this).has('.popup_box_wrap').css('zIndex','-1');
 
     });
    
