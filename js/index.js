d4248012e66e6bd9cb72686f2d14ac972d0073d9
LimjooHyun <99496403+uhzill@users.noreply.github.com>
2022-12-12 00:31:07 +0900

    10

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    
+    $(".ripples").ripples({
+        resolution: 256, // 렌더링 값이 클수록 잔물결 효과가 느리게 전파
+        perturbance: 0.5, // 잔물경 굴절 강도. 0은 굴절 없음
+    });
+
+});
